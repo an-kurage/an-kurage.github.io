3f85849a1ae0bd5868adb8256d71882fcd87a832
あんくらげ (ankurage) <supply497@gmail.com>
2026-04-25 22:42:22 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function burgermenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
