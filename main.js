cf25fb3739858900beb8cefba2144986a5a3a230
あんくらげ (ankurage) <supply497@gmail.com>
2026-05-31 23:01:20 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 function burgermenu() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,3 +8,11 @@ function burgermenu() {
     x.className = "topnav";
   }
 }
+
+document.addEventListener('click', (event) => {
+  var x = document.getElementById("myTopnav");
+  var b = document.getElementById("myicon");
+  if(!x.contains(event.target) && !b.contains(event.target)) {
+    x.className = "topnav";
+  }
+});
